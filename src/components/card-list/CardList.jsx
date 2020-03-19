621167b47c1d14a12ea1b2abd32560ce552e5b23
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-19 12:38:39 +0000

    Adding files for github pages

---
+++ src/components/card-list/CardList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CardList.styles.css";
+import Card from "../card/Card";
+
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Card.styles.css";
+
+const Card = ({ monster }) => {
+  return (
+    <div className="card-container">
+      <img
+        src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+        alt="monster"
+      />
+      <h2>{monster.name}</h2>
+      <p>{monster.email}</p>
+    </div>
+  );
+};
+
+export default Card;
