6fa480dd98012fe74b44436ef616577f40f53070
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-19 12:36:46 +0000

    Updates

---
+++ precache-manifest.2091e55bb8c5b3eebdb16f56186f1eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cce4d266ff604741eb54e295e011165",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "af64f0598a79f9b79f7c",
+    "url": "/monsters-rolodex/static/css/main.1e1804b4.chunk.css"
+  },
+  {
+    "revision": "05ccdba27653d5578269",
+    "url": "/monsters-rolodex/static/js/2.4f11b679.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.4f11b679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af64f0598a79f9b79f7c",
+    "url": "/monsters-rolodex/static/js/main.0f57a1ee.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
